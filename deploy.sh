3a347763ee134f3b7d2cb1cceb8638155f68ac02
Chris Klosowski <cklosowski@gmail.com>
2014-02-24 22:55:55 -0700

    Fixing deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 # main config
 PLUGINSLUG=${PWD##*/} # returns basename of current directory
 CURRENTDIR=`pwd`
-MAINFILE="pushover-notifications.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="edd-additional-shortcodes.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
@@ -15,7 +15,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/$PLUGINSLUG" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="cklosowski" # your svn username
+SVNUSER="cklosows" # your svn username
 
 
 # Let's begin...
